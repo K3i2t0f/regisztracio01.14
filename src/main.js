5f3de7ee6a1cb3aa0b237c806ffb4b5b0aa42c51
K3i2t0f <borbarakika@gmail.com>
2026-01-14 11:35:22 +0100

    js része kész.

---
+++ src/main.js
@@ -0,0 +1,11 @@
+
+const form = document.getElementById('registrationForm');
+const successMessage = document.getElementById('successMessage');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+
+  form.reset();
+
+  successMessage.textContent = "Sikeres regisztráció!";
+});
